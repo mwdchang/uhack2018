8472a00b88695300a15dac73310df31b54c32589
Mark <mdeitner@uncharted.software>
2018-07-12 00:48:39 -0400

    Add chem labels with fonts

--- src/components/map-panel.vue
+++ src/components/map-panel.vue
@@ -19,7 +19,7 @@
       this.waterMarkers = L.layerGroup();
 
       this.createMap();
-      this.addLocationMarkers();
+      this.addLocationMarkers(this.waters);
     },
     computed: {
       ...mapGetters({
@@ -52,21 +52,37 @@
         this.tileLayer = L.tileLayer(
           'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
           {
-              maxZoom: 18,
-              attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+            maxZoom: 18,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
           }
         );
         this.tileLayer.addTo(this.map);
       },
 
-      addLocationMarkers() {
+      addLocationMarkers(waters) {
         const CHEMS = {
           'ARSENIC': 'Ar',
           'CHROMIUM': 'Cr',
           'LEAD': 'Pb'
         };
         let uniqueWaters = {};
-        this.waters.forEach( water => {
+        let curMaxVals = {
+          'ARSENIC': 0,
+          'CHROMIUM': 0,
+          'LEAD': 0
+        };
+        let deltaMaxVals = {
+          'ARSENIC': 0,
+          'CHROMIUM': 0,
+          'LEAD': 0
+        };
+        waters.forEach( water => {
+          if (water.data[water.data.length - 1] > curMaxVals[water.chemical]) {
+            curMaxVals[water.chemical] = water.data[water.data.length - 1];
+          }
+          if (water.data[water.data.length - 1] - water.data[water.data.length - 2] > deltaMaxVals[water.chemical]) {
+            deltaMaxVals[water.chemical] = water.data[water.data.length - 1] - water.data[water.data.length - 2];
+          }
           if (water.id in uniqueWaters) {
             uniqueWaters[water.id].push(water);
           } else {
@@ -74,14 +90,21 @@
           }
         });
         Object.keys(uniqueWaters).forEach((k) => {
-          let label = ''
+          let label = '';
           uniqueWaters[k].forEach((chem) => {
-            label += CHEMS[chem.chemical];
+            let weight = chem.data[chem.data.length - 1] / (curMaxVals[chem.chemical]);
+            weight = weight < 0 ? 0 : weight;
+            weight = weight * 900;
+
+            let size = (chem.data[chem.data.length - 1] - chem.data[chem.data.length - 2]) / (deltaMaxVals[chem.chemical]);
+            size = size < 0 ? 0 : size;
+            size = size * 8 + 12;
+            label += '<span style="font-weight: ' + weight + '; font-size: '+size+'px" class="location-marker-text">' + CHEMS[chem.chemical] + '</span>';
           });
           let divIcon = L.divIcon({
             className:'water-marker-div-icon',
             //html:'<i class="fa fa-tint fa-2x"></i><span class="location-marker-text">' + uniqueWaters[k][0].name + '</span>',
-            html:'<i class="fa fa-tint fa-2x"></i><span class="location-marker-text">' + label + '</span>',
+            html:'<i class="fa fa-tint fa-2x"></i>' + label,
             iconAnchor:[14,14],
             iconSize:null,
             popupAnchor:[0,0]
@@ -106,8 +129,7 @@
         const loc = this.currentLocation;
         const origin = new L.LatLng(loc.lat, loc.lon);
 
-
-        this.map.setView(origin, 8);  // zoom of 8 gives us about 60 km radius
+        this.map.setView(origin, 10);  // zoom of 8 gives us about 60 km radius
 
         // TODO
         const weightFn = (f) => {
@@ -122,28 +144,27 @@
             const pointList = [origin, fLoc];
             const line = new L.polyline(pointList, {
               color: '#555',
-              weight: weightFn(facility),
+              weight: 2, //weightFn(facility),
               opacity: 0.5,
               smoothFactor: 1
             });
             this.cluster.addLayer(line);
+
+            let divIcon =L.divIcon({
+              className:'facility-marker-div-icon',
+              //html:'<i class="fa fa-industry fa-2x"></i><span class="location-marker-text">' + facility.name + '</span>',
+              html:'<i class="fa fa-industry fa-2x"><span class="location-marker-text">' + facility.name + '</span></i>',
+              iconAnchor:[14,14],
+              iconSize:null,
+              popupAnchor:[0,0]
+            });
+
+            let marker = L.marker([facility.lat, facility.lon], {
+              icon: divIcon,
+              //opacity: 0.3,
+            });
+            this.cluster.addLayer(marker);
           }
-
-          let divIcon =L.divIcon({
-            className:'facility-marker-div-icon',
-            //html:'<i class="fa fa-industry fa-2x"></i><span class="location-marker-text">' + facility.name + '</span>',
-            html:'<i class="fa fa-industry fa-2x"><span class="location-marker-text">' + facility.name + '</span></i>',
-            iconAnchor:[14,14],
-            iconSize:null,
-            popupAnchor:[0,0]
-          });
-
-          let marker = L.marker([facility.lat, facility.lon], {
-            icon: divIcon,
-            //opacity: 0.3,
-          });
-          this.cluster.addLayer(marker);
-
         })
         this.cluster.addTo(this.map);
       }
@@ -152,38 +173,38 @@
 </script>
 
 <style>
-.map-panel {
-  position: relative;
-  display: block;
-  box-sizing: border-box;
-  border: 1px solid #ccc;
-  margin: 2px;
+  .map-panel {
+    position: relative;
+    display: block;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    margin: 2px;
 
-  min-height: 600px;
-  width: 100%;
-}
+    min-height: 600px;
+    width: 100%;
+  }
 
-.water-marker-div-icon {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  color: blue;
-  white-space: nowrap;
-}
+  .water-marker-div-icon {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    color: blue;
+    white-space: nowrap;
+  }
 
-.facility-marker-div-icon {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  color: brown;
-  white-space: nowrap;
-}
+  .facility-marker-div-icon {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    color: brown;
+    white-space: nowrap;
+  }
 
-.location-marker-text {
-  padding-left: 5px;
-  color: #555;
-  font-size: 14px;
-  font-weight: 600;
-}
+  .location-marker-text {
+    padding-left: 5px;
+    color: #555;
+  //font-size: 14px;
+  //font-weight: 600;
+  }
 
 </style>
